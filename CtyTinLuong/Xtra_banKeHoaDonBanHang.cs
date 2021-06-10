f463a43a7138a2ef5a2773939aae65be5ed2134f
taquanghuu <huutaa88@gmail.com>
2021-06-10 13:15:42 +0700

    x

---
+++ CtyTinLuong/Xtra_banKeHoaDonBanHang.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace CtyTinLuong
+{
+    public partial class Xtra_banKeHoaDonBanHang : DevExpress.XtraReports.UI.XtraReport
+    {
+        public Xtra_banKeHoaDonBanHang()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
