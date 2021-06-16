35c750dee49a016a9c578c7be5507cc8158db436
huutq-88 <84846147+huutq-88@users.noreply.github.com>
2021-06-16 07:52:16 +0700

    d

--- CtyTinLuong/KhoDaiLy/frmBaoCao_Nhap_Xuat_ton_kho_DaiLy.cs
+++ CtyTinLuong/KhoDaiLy/frmBaoCao_Nhap_Xuat_ton_kho_DaiLy.cs
@@ -16,5 +16,26 @@ namespace CtyTinLuong
         {
             InitializeComponent();
         }
+
+        private void frmBaoCao_Nhap_Xuat_ton_kho_DaiLy_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btRefresh_Click(object sender, EventArgs e)
+        {
+            frmBaoCao_Nhap_Xuat_ton_kho_DaiLy_Load( sender,  e);
+        }
+
+        private void btThoat2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void bandedGridView1_CustomDrawCell(object sender, DevExpress.XtraGrid.Views.Base.RowCellCustomDrawEventArgs e)
+        {
+            if (e.Column == clSTT)
+                e.DisplayText = (e.RowHandle + 1).ToString();
+        }
     }
 }
