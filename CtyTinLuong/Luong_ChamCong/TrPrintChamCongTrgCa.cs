ab418071b96fcc6a036d225f631c3bc51bf8344b
trieunv <ngvtrieusqtt@gmail.com>
2021-06-24 02:31:59 +0700

    t

---
+++ CtyTinLuong/Luong_ChamCong/TrPrintChamCongTrgCa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace CtyTinLuong.Luong_ChamCong
+{
+    public partial class TrPrintChamCongTrgCa : DevExpress.XtraReports.UI.XtraReport
+    {
+        public TrPrintChamCongTrgCa()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
