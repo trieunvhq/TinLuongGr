29012c9c9b0cce88b1bcea09aa0fce02fdf27646
trieunv <ngvtrieusqtt@gmail.com>
2021-06-24 15:59:32 +0700

    Update TrPrintChamCongTrgCa.Designer.cs

--- CtyTinLuong/Luong_ChamCong/TrPrintChamCongTrgCa.Designer.cs
+++ CtyTinLuong/Luong_ChamCong/TrPrintChamCongTrgCa.Designer.cs
@@ -750,15 +750,15 @@
             // xrLabel2
             // 
             this.xrLabel2.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold);
-            this.xrLabel2.LocationFloat = new DevExpress.Utils.PointFloat(0F, 1.83334F);
+            this.xrLabel2.LocationFloat = new DevExpress.Utils.PointFloat(0F, 1.833344F);
             this.xrLabel2.Multiline = true;
             this.xrLabel2.Name = "xrLabel2";
             this.xrLabel2.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F);
-            this.xrLabel2.SizeF = new System.Drawing.SizeF(1055F, 22.99998F);
+            this.xrLabel2.SizeF = new System.Drawing.SizeF(1055F, 27.16665F);
             this.xrLabel2.StylePriority.UseFont = false;
             this.xrLabel2.StylePriority.UseTextAlignment = false;
             this.xrLabel2.Text = "BẢNG CHẤM CÔNG TRƯỞNG CA";
-            this.xrLabel2.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopCenter;
+            this.xrLabel2.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter;
             // 
             // xrlbNam
             // 
