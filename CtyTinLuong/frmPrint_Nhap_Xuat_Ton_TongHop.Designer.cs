cd6dd0a83b0f639b929f69086694a9cafa07c023
trieunv <ngvtrieusqtt@gmail.com>
2021-06-25 10:18:47 +0700

    t

--- CtyTinLuong/frmPrint_Nhap_Xuat_Ton_TongHop.Designer.cs
+++ CtyTinLuong/frmPrint_Nhap_Xuat_Ton_TongHop.Designer.cs
@@ -28,10 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrint_Nhap_Xuat_Ton_TongHop));
             this.documentViewer1 = new DevExpress.XtraPrinting.Preview.DocumentViewer();
-            this.documentViewerBarManager1 = new DevExpress.XtraPrinting.Preview.DocumentViewerBarManager(this.components);
+            this.documentViewerBarManager1 = new DevExpress.XtraPrinting.Preview.DocumentViewerBarManager();
             this.previewBar1 = new DevExpress.XtraPrinting.Preview.PreviewBar();
             this.printPreviewBarItem2 = new DevExpress.XtraPrinting.Preview.PrintPreviewBarItem();
             this.printPreviewBarItem3 = new DevExpress.XtraPrinting.Preview.PrintPreviewBarItem();
@@ -912,8 +911,9 @@
             this.Controls.Add(this.barDockControlRight);
             this.Controls.Add(this.barDockControlBottom);
             this.Controls.Add(this.barDockControlTop);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmPrint_Nhap_Xuat_Ton_TongHop";
-            this.Text = "Báo cáo NXT";
+            this.Text = "Báo Cáo NXT";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmPrint_Nhap_Xuat_Ton_TongHop_FormClosed);
             this.Load += new System.EventHandler(this.frmPrint_Nhap_Xuat_Ton_TongHop_Load);
