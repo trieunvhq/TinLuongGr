0e00acf877eabb49738cd2a741ded00451cdb469
trieunv <ngvtrieusqtt@gmail.com>
2021-06-25 10:52:16 +0700

    t

--- CtyTinLuong/KhoBanThanhPham/KhoBTP_ChiTiet_NhapKho_Khac.Designer.cs
+++ CtyTinLuong/KhoBanThanhPham/KhoBTP_ChiTiet_NhapKho_Khac.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             DevExpress.XtraEditors.Controls.EditorButtonImageOptions editorButtonImageOptions1 = new DevExpress.XtraEditors.Controls.EditorButtonImageOptions();
             DevExpress.Utils.SerializableAppearanceObject serializableAppearanceObject1 = new DevExpress.Utils.SerializableAppearanceObject();
             DevExpress.Utils.SerializableAppearanceObject serializableAppearanceObject2 = new DevExpress.Utils.SerializableAppearanceObject();
@@ -653,7 +652,10 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "KhoBTP_ChiTiet_NhapKho_Khac";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Nhập Kho Khác";
             this.Load += new System.EventHandler(this.KhoBTP_ChiTiet_NhapKho_Khac_Load);
             ((System.ComponentModel.ISupportInitialize)(this.mrmVTHH)).EndInit();
