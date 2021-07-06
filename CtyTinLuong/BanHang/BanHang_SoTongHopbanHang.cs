dd74eca1cdebc8824af417f2cd242c0a56a540d3
taquanghuu <huutaa88@gmail.com>
2021-07-06 20:31:24 +0700

    k

--- CtyTinLuong/BanHang/BanHang_SoTongHopbanHang.cs
+++ CtyTinLuong/BanHang/BanHang_SoTongHopbanHang.cs
@@ -258,40 +258,83 @@ namespace CtyTinLuong
 
         private void btPrint_KH_Click(object sender, EventArgs e)
         {
-            mdtPrint = new DataTable();
-            DataTable DatatableABC = (DataTable)gridControl1.DataSource;
-            CriteriaOperator op = gridView1.ActiveFilterCriteria; // filterControl1.FilterCriteria
-            string filterString = DevExpress.Data.Filtering.CriteriaToWhereClauseHelper.GetDataSetWhere(op);
-            DataView dv1212 = new DataView(DatatableABC);
-            dv1212.RowFilter = filterString;
-            mdtPrint = dv1212.ToTable();
 
-            if (mdtPrint.Rows.Count > 0)
-            {
-                mbPrint_KhachHang = true;
-                mbPrint_RutGon = false;
-                mbPrint_ALL = false;
-                mdatungay = dteTuNgay.DateTime;
-                mdadenngay = dteDenNgay.DateTime;
-                clsBanHang_tbBanHang cls = new clsBanHang_tbBanHang();               
-                DataTable dt = new DataTable();
-                int iiIID_KhachHang = Convert.ToInt32(gridKH.EditValue.ToString());
-                if (iiIID_KhachHang == 0)
-                    dt = cls.SA_Time_DS_KH(mdatungay, mdadenngay);
-                else
-                {
-
-                }
-                frmPrint_baoGia_BanHanag ff = new frmPrint_baoGia_BanHanag();
-                ff.ShowDialog();
-
-            }
+            mbPrint_KhachHang = true;
+            mbPrint_RutGon = false;
+            mbPrint_ALL = false;
+            mdatungay = dteTuNgay.DateTime;
+            mdadenngay = dteDenNgay.DateTime;
+            clsBanHang_tbBanHang cls = new clsBanHang_tbBanHang();
+            DataTable dt1 = new DataTable();
+            int iiIID_KhachHang = Convert.ToInt32(gridKH.EditValue.ToString());
+            if (iiIID_KhachHang == 0)
+                dt1 = cls.SA_Time_DS_KH(mdatungay, mdadenngay);
             else
             {
-                MessageBox.Show("không có dữ liệu");
+               
+                DataRow _ravi2 = dt1.NewRow();
+                _ravi2["ID_KhachHang"] = iiIID_KhachHang;
+                _ravi2["MaKH"] = gridKH.Text.ToString();
+                _ravi2["TenKH"] = txtTenKH.Text.ToString();
+                dt1.Rows.Add(_ravi2);
+            }
+            mdtPrint = new DataTable();
+            mdtPrint.Columns.Add("MaVT", typeof(string));
+            mdtPrint.Columns.Add("TenVTHH", typeof(string));
+            mdtPrint.Columns.Add("DonViTinh", typeof(string));
+            mdtPrint.Columns.Add("TenKH", typeof(string));
+            mdtPrint.Columns.Add("SoLuong", typeof(string));     
+            mdtPrint.Columns.Add("TongTienUSD", typeof(string));
+            mdtPrint.Columns.Add("MaKH", typeof(string));
+            mdtPrint.Columns.Add("TenKH", typeof(string));
+            for (int i = 0; i < dt1.Rows.Count; i++)
+            {
+
+                int xxID_Khachhang_ = Convert.ToInt32(dt1.Rows[i]["ID_KhachHang"].ToString());
+                cls = new clsBanHang_tbBanHang();
+                DataTable dt2 = cls.SA_distinct_ID_VTHH_ID_KhachHang(xxID_Khachhang_, dteTuNgay.DateTime, dteDenNgay.DateTime);
+                if (dt2.Rows.Count > 0)
+                {
+                    string hienthi = "1";
+                    double TongTienUSDxx = Convert.ToDouble(dt2.Compute("sum(ThanhTien)", "HienThi="+ hienthi + ""));
+                   
+                    for (int k = 0; k < dt2.Rows.Count; k++)
+                    {
+                        DataRow _ravi = mdtPrint.NewRow();
+                        _ravi["MaKH"] = dt1.Rows[i]["MaKH"].ToString();
+                        _ravi["TenKH"] = dt1.Rows[i]["TenKH"].ToString();
+                        _ravi["MaVT"] = dt2.Rows[k]["MaVT"].ToString();
+                        _ravi["TenVTHH"] = dt2.Rows[k]["TenVTHH"].ToString();
+                        _ravi["DonViTinh"] = dt2.Rows[k]["DonViTinh"].ToString();                        
+                        _ravi["SoLuong"] = dt2.Rows[k]["SoLuong"].ToString();                   
+                        _ravi["TongTienUSD"] = TongTienUSDxx;
+                        mdtPrint.Rows.Add(_ravi);
+                    }
+                }
+
+
+                if (mdtPrint.Rows.Count > 0)
+                {
+                    mbPrint_KhachHang = false;
+                    mbPrint_RutGon = false;
+                    mbPrint_ALL = true;
+                    mdatungay = dteTuNgay.DateTime;
+                    mdadenngay = dteDenNgay.DateTime;
+                    frmPrint_baoGia_BanHanag ff = new frmPrint_baoGia_BanHanag();
+                    ff.ShowDialog();
+
+                }
+
             }
 
-           
+
+
+            //else
+            //{
+            //    MessageBox.Show("không có dữ liệu");
+            //}
+
+
         }
 
         private void BanHang_SoTongHopbanHang_Load(object sender, EventArgs e)
--- CtyTinLuong/BanHang/frmPrint_baoGia_BanHanag.cs
+++ CtyTinLuong/BanHang/frmPrint_baoGia_BanHanag.cs
@@ -103,15 +103,7 @@ namespace CtyTinLuong
                 _ravi["DonViTinh"] = dt3.Rows[i]["DonViTinh"].ToString();
                 _ravi["TenKH"] = dt3.Rows[i]["TenKH"].ToString();
                 _ravi["SoLuong"] = dt3.Rows[i]["SoLuong"].ToString();
-                _ravi["DonGia"] = dt3.Rows[i]["DonGia"].ToString();
-                _ravi["ThanhTien"] = dt3.Rows[i]["ThanhTien"].ToString();
-                _ravi["TiGia"] = dt3.Rows[i]["TiGia"].ToString();
                 _ravi["TongTienUSD"] = dt3.Rows[i]["TongTienUSD"].ToString();
-                _ravi["TongTienVND"] = dt3.Rows[i]["TongTienVND"].ToString();
-                _ravi["QuyDoiVND"] = dt3.Rows[i]["QuyDoiVND"].ToString();
-                _ravi["SoChungTu"] = dt3.Rows[i]["SoChungTu"].ToString();
-                _ravi["NgayChungTu"] = dt3.Rows[i]["NgayChungTu"].ToString();
-
                 ds.tbBan_MuaHang.Rows.Add(_ravi);
             }
             xtr111.DataSource = null;
