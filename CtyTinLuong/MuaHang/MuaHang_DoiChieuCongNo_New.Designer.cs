e38fe1f32ff3ea5c84a4f8b0d9a997326124fcac
huutq-88 <84846147+huutq-88@users.noreply.github.com>
2021-07-08 10:55:53 +0700

    dsdsd

---
+++ CtyTinLuong/MuaHang/MuaHang_DoiChieuCongNo_New.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace CtyTinLuong
+{
+    partial class MuaHang_DoiChieuCongNo_New
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MuaHang_DoiChieuCongNo_New
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Name = "MuaHang_DoiChieuCongNo_New";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đối chiếu công nợ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
