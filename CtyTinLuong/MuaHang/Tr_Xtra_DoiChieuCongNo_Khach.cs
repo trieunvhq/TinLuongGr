b46b1398236f86418e6b48ebedf7e83eb6f25f71
trieunv <ngvtrieusqtt@gmail.com>
2021-07-08 14:37:17 +0700

    t

---
+++ CtyTinLuong/MuaHang/Tr_Xtra_DoiChieuCongNo_Khach.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+using System.Data;
+
+namespace CtyTinLuong
+{
+    public partial class Tr_Xtra_DoiChieuCongNo_Khach : DevExpress.XtraReports.UI.XtraReport
+    {
+        DateTime _tuNgay, _denNgay;
+        public Tr_Xtra_DoiChieuCongNo_Khach(DateTime tuNgay, DateTime denNgay)
+        {
+            _tuNgay = tuNgay;
+            _denNgay = denNgay;
+            InitializeComponent();
+            pNgayThang.Value = DateTime.Now;
+        }
+
+        private void ReportHeader_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            clsAaatbMacDinhNguoiKy cls = new CtyTinLuong.clsAaatbMacDinhNguoiKy();
+            cls.iID_DangNhap = frmDangNhap.miID_DangNhap;
+            DataTable dt = cls.SelectAll_ID_DangNhap();
+            if (dt.Rows.Count > 0)
+            {
+                pNguoiLap.Value = dt.Rows[1]["HoTen"].ToString();
+                pTongHop.Value = dt.Rows[4]["HoTen"].ToString();
+                pPhoGiamDoc.Value = dt.Rows[8]["HoTen"].ToString();
+            }
+
+            lbTitleNgay.Text = "Từ ngày " + _tuNgay.ToString("dd/MM/yyyy") + " đến ngày " + _denNgay.ToString("dd/MM/yyyy");
+
+            //Load label ngay ky footer:
+            DateTime d = Convert.ToDateTime(pNgayThang.Value);
+            if (d.Day > 9)
+            {
+                if (d.Month > 9)
+                {
+                    lbNgayThangNam.Text = "Ngày " + d.Day
+                                        + " tháng " + d.Month
+                                        + " năm " + d.Year;
+                }
+                else
+                {
+                    lbNgayThangNam.Text = "Ngày " + d.Day
+                                        + " tháng 0" + d.Month
+                                        + " năm " + d.Year;
+                }
+
+            }
+            else
+            {
+                if (d.Month > 9)
+                {
+                    lbNgayThangNam.Text = "Ngày 0" + d.Day
+                                        + " tháng " + d.Month
+                                        + " năm " + d.Year;
+                }
+                else
+                {
+                    lbNgayThangNam.Text = "Ngày 0" + d.Day
+                                        + " tháng 0" + d.Month
+                                        + " năm " + d.Year;
+                }
+            }
+
+        }
+    }
+}
---
+++ CtyTinLuong/MuaHang/Tr_frmPrint_DoiChieuCongNo_Khach.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CtyTinLuong
+{
+    public partial class Tr_frmPrint_DoiChieuCongNo_Khach : Form
+    {
+
+        public DateTime _tuNgay, _denNgay;
+        private DataTable _data;
+
+
+        public Tr_frmPrint_DoiChieuCongNo_Khach(DateTime tuNgay, DateTime denNgay, DataTable data)
+        {
+            _data = data;
+            _tuNgay = tuNgay;
+            _denNgay = denNgay;
+
+            InitializeComponent();
+        }
+
+        //
+        private void Tr_frmPrint_DoiChieuCongNo_Khach_Load(object sender, EventArgs e)
+        {
+            DataSet_TinLuong ds = new DataSet_TinLuong();
+           
+            for (int i = 0; i < _data.Rows.Count; ++i)
+            {
+                DataRow _ravi = ds.tbBH_DoiChieuCongNo_NCC.NewRow();
+
+                //
+                _ravi["STT"] = (i + 1).ToString();
+                _ravi["DoiTuong"] = _data.Rows[i]["DoiTuong"].ToString();
+                _ravi["NgayThang"] = _data.Rows[i]["NgayThang"].ToString();
+                _ravi["DienGiai"] = _data.Rows[i]["DienGiai"].ToString();
+
+                if (_data.Rows[i]["SoLuong"].ToString() == "")
+                {
+                    _ravi["SoLuong"] = "";
+                }
+                else
+                {
+                    _ravi["SoLuong"] = Convert.ToDouble(_data.Rows[i]["SoLuong"].ToString()).ToString("N2");
+                }
+
+                //
+                if (_data.Rows[i]["DonGia"].ToString() == "")
+                {
+                    _ravi["DonGia"] = "";
+                }
+                else
+                {
+                    _ravi["DonGia"] = Convert.ToDouble(_data.Rows[i]["DonGia"].ToString()).ToString("N2");
+                }
+
+                //
+                if (_data.Rows[i]["ThanhTien"].ToString() == "")
+                {
+                    _ravi["ThanhTien"] = "";
+                }
+                else
+                {
+                    _ravi["ThanhTien"] = Convert.ToDouble(_data.Rows[i]["ThanhTien"].ToString()).ToString("N2");
+                }
+
+
+                //
+                if (_data.Rows[i]["No"].ToString() == "")
+                {
+                    _ravi["No"] = "";
+                }
+                else
+                {
+                    _ravi["No"] = Convert.ToDouble(_data.Rows[i]["No"].ToString()).ToString("N2");
+                }
+
+
+                //
+                if (_data.Rows[i]["Co"].ToString() == "")
+                {
+                    _ravi["Co"] = "";
+                }
+                else
+                {
+                    _ravi["Co"] = Convert.ToDouble(_data.Rows[i]["Co"].ToString()).ToString("N2");
+                }
+
+                ds.tbBH_DoiChieuCongNo_NCC.Rows.Add(_ravi);
+            }
+
+            Tr_Xtra_DoiChieuCongNo_NCC xtr111 = new Tr_Xtra_DoiChieuCongNo_NCC(_tuNgay, _denNgay);
+            xtr111.DataSource = null;
+            xtr111.DataSource = ds.tbBH_DoiChieuCongNo_NCC;
+            xtr111.DataMember = "tbBH_DoiChieuCongNo_NCC";
+            xtr111.CreateDocument();
+            documentViewer1.DocumentSource = xtr111;
+        }
+    }
+}
