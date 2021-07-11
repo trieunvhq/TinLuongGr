4125f24d02ac7bfe85e38f6902a9ab951912e470
taquanghuu <huutaa88@gmail.com>
2021-07-11 14:55:18 +0700

    s

--- CtyTinLuong/KhoThanhPham/frmQuanLyKhoThanhPham.Designer.cs
+++ CtyTinLuong/KhoThanhPham/frmQuanLyKhoThanhPham.Designer.cs
@@ -50,6 +50,7 @@
             this.navBarItem3 = new DevExpress.XtraNavBar.NavBarItem();
             this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.navBarItem11 = new DevExpress.XtraNavBar.NavBarItem();
             ((System.ComponentModel.ISupportInitialize)(this.navBarControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
             this.SuspendLayout();
@@ -76,7 +77,8 @@
             this.navBarItem8,
             this.navBarItem1,
             this.navBarItem13,
-            this.navBarItem10});
+            this.navBarItem10,
+            this.navBarItem11});
             this.navBarControl1.Location = new System.Drawing.Point(6, 13);
             this.navBarControl1.Name = "navBarControl1";
             this.navBarControl1.OptionsNavPane.ExpandedWidth = 216;
@@ -91,7 +93,8 @@
             this.navBarGroup1.ItemLinks.AddRange(new DevExpress.XtraNavBar.NavBarItemLink[] {
             new DevExpress.XtraNavBar.NavBarItemLink(this.navChoNhapKho_tuDaiLy),
             new DevExpress.XtraNavBar.NavBarItemLink(this.navDaNhapKho),
-            new DevExpress.XtraNavBar.NavBarItemLink(this.navBarItem1)});
+            new DevExpress.XtraNavBar.NavBarItemLink(this.navBarItem1),
+            new DevExpress.XtraNavBar.NavBarItemLink(this.navBarItem11)});
             this.navBarGroup1.Name = "navBarGroup1";
             // 
             // navChoNhapKho_tuDaiLy
@@ -217,6 +220,12 @@
             this.linkLabel1.Text = "Cài mặc định người ký";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
+            // navBarItem11
+            // 
+            this.navBarItem11.Caption = "Nhập kho từ gấp dán";
+            this.navBarItem11.Name = "navBarItem11";
+            this.navBarItem11.LinkClicked += new DevExpress.XtraNavBar.NavBarLinkEventHandler(this.navBarItem11_LinkClicked);
+            // 
             // frmQuanLyKhoThanhPham
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -231,7 +240,7 @@
             this.MinimizeBox = false;
             this.Name = "frmQuanLyKhoThanhPham";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = this.Name+"-->Kho Thành Phẩm";
+            this.Text = "frmQuanLyKhoThanhPham-->Kho Thành Phẩm";
             this.Load += new System.EventHandler(this.frmQuanLyKhoThanhPham_Load);
             ((System.ComponentModel.ISupportInitialize)(this.navBarControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
@@ -263,5 +272,6 @@
         private DevExpress.XtraNavBar.NavBarItem navBarItem13;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private DevExpress.XtraNavBar.NavBarItem navBarItem10;
+        private DevExpress.XtraNavBar.NavBarItem navBarItem11;
     }
 }
--- CtyTinLuong/KhoThanhPham/frmQuanLyKhoThanhPham.cs
+++ CtyTinLuong/KhoThanhPham/frmQuanLyKhoThanhPham.cs
@@ -166,5 +166,14 @@ namespace CtyTinLuong
             ((NavBarItem)sender).Appearance.ForeColor = Color.Blue;
             ((NavBarItem)sender).Appearance.Font = new Font("Tahoma", 8.25F, FontStyle.Bold);
         }
+
+        private void navBarItem11_LinkClicked(object sender, NavBarLinkEventArgs e)
+        {
+            doiMauTitle(sender, e);
+            UCThanhPham_NhapKhoTu_GapDan uccc_NhapKho = new UCThanhPham_NhapKhoTu_GapDan();
+            uccc_NhapKho.Dock = DockStyle.Fill;
+            panelControl1.Controls.Add(uccc_NhapKho);
+            uccc_NhapKho.BringToFront();
+        }
     }
 }
