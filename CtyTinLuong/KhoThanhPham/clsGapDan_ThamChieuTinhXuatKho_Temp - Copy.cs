c95aebdc2d74c19371cd76c202969a635f05583d
taquanghuu <huutaa88@gmail.com>
2021-07-11 15:28:17 +0700

    Update clsGapDan_ThamChieuTinhXuatKho_Temp - Copy.cs

--- CtyTinLuong/KhoThanhPham/clsGapDan_ThamChieuTinhXuatKho_Temp - Copy.cs
+++ CtyTinLuong/KhoThanhPham/clsGapDan_ThamChieuTinhXuatKho_Temp - Copy.cs
@@ -7,11 +7,11 @@ namespace CtyTinLuong
 {
 	public partial class clsGapDan_ThamChieuTinhXuatKho_Temp : clsDBInteractionBase
 	{
-        public void Delete_ALL_W_ID_XuatKhoDaiLy(int xxiID_XuatKhoDaiLy_)
+        public void Delete_ALL_W_ID_XuatKhoDaiLy(int xxiID_XuatKho_)
         {
 
             SqlCommand scmCmdToExecute = new SqlCommand();
-            scmCmdToExecute.CommandText = "dbo.[pr_DaiLy_ThamChieu_TinhXuatKho_Temp_Delete_ALL_W_ID_XuatKhoDaiLy]";
+            scmCmdToExecute.CommandText = "dbo.[pr_GapDan_ThamChieuTinhXuatKho_Temp_Delete_ALL_W_ID_XuatKho]";
             scmCmdToExecute.CommandType = CommandType.StoredProcedure;
 
             // Use base class' connection object
@@ -20,18 +20,8 @@ namespace CtyTinLuong
             try
             {
 
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@iID_ThamChieu", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, m_iID_ThamChieu));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@iID_DaiLy", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, m_iID_DaiLy));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@iID_NhapKhoDaiLy", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, m_iID_NhapKhoDaiLy));
-                scmCmdToExecute.Parameters.Add(new SqlParameter("@iID_XuatKhoDaiLy", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, xxiID_XuatKhoDaiLy_));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@iID_VTHH", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, m_iID_VTHH));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@fSoLuongThanhPhamQuyDoi", SqlDbType.Float, 8, ParameterDirection.Input, false, 38, 0, "", DataRowVersion.Proposed, m_fSoLuongThanhPhamQuyDoi));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@fSoLuongNhap", SqlDbType.Float, 8, ParameterDirection.Input, false, 38, 0, "", DataRowVersion.Proposed, m_fSoLuongNhap));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@fDonGia", SqlDbType.Float, 8, ParameterDirection.Input, false, 38, 0, "", DataRowVersion.Proposed, m_fDonGia));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@bTonTai", SqlDbType.Bit, 1, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, xxTontai_));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@bNgungTheoDoi", SqlDbType.Bit, 1, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, m_bNgungTheoDoi));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@iErrorCode", SqlDbType.Int, 4, ParameterDirection.Output, false, 10, 0, "", DataRowVersion.Proposed, m_iErrorCode));
-
+                scmCmdToExecute.Parameters.Add(new SqlParameter("@iID_XuatKho_", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, xxiID_XuatKho_));
+                
                 m_scoMainConnection.Open();
 
                 // Execute query.
@@ -41,7 +31,7 @@ namespace CtyTinLuong
             catch (Exception ex)
             {
                 // some error occured. Bubble it to caller and encapsulate Exception object
-                throw new Exception("pr_DaiLy_ThamChieu_TinhXuatKho_Temp_Delete_ALL_W_ID_XuatKhoDaiLy::Error occured.", ex);
+                throw new Exception("pr_GapDan_ThamChieuTinhXuatKho_Temp_Delete_ALL_W_ID_XuatKho::Error occured.", ex);
             }
             finally
             {
@@ -96,9 +86,9 @@ namespace CtyTinLuong
         public DataTable SA_NgayThang()
         {
             SqlCommand scmCmdToExecute = new SqlCommand();
-            scmCmdToExecute.CommandText = "dbo.[pr_DaiLy_ThamChieu_TinhXuatKho_Temp_SA_NgayThang]";
+            scmCmdToExecute.CommandText = "dbo.[pr_GapDan_ThamChieuTinhXuatKho_Temp_SA_NgayThang]";
             scmCmdToExecute.CommandType = CommandType.StoredProcedure;
-            DataTable dtToReturn = new DataTable("pr_DaiLy_ThamChieu_TinhXuatKho_Temp_SA_NgayThang");
+            DataTable dtToReturn = new DataTable("pr_GapDan_ThamChieuTinhXuatKho_Temp_SA_NgayThang");
             SqlDataAdapter sdaAdapter = new SqlDataAdapter(scmCmdToExecute);
 
             // Use base class' connection object
@@ -107,49 +97,6 @@ namespace CtyTinLuong
             try
             {
                 m_scoMainConnection.Open();
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@iID_ThamChieu", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, m_iID_ThamChieu));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@iID_DaiLy", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, m_iID_DaiLy));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@iID_NhapKhoDaiLy", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, m_iID_NhapKhoDaiLy));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@iID_XuatKhoDaiLy", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, xxiID_XuatKhoDaiLy_));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@iID_VTHH", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, m_iID_VTHH));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@fSoLuongThanhPhamQuyDoi", SqlDbType.Float, 8, ParameterDirection.Input, false, 38, 0, "", DataRowVersion.Proposed, m_fSoLuongThanhPhamQuyDoi));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@fSoLuongNhap", SqlDbType.Float, 8, ParameterDirection.Input, false, 38, 0, "", DataRowVersion.Proposed, m_fSoLuongNhap));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@fDonGia", SqlDbType.Float, 8, ParameterDirection.Input, false, 38, 0, "", DataRowVersion.Proposed, m_fDonGia));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@bTonTai", SqlDbType.Bit, 1, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, m_bTonTai));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@bNgungTheoDoi", SqlDbType.Bit, 1, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, m_bNgungTheoDoi));
-                //scmCmdToExecute.Parameters.Add(new SqlParameter("@iErrorCode", SqlDbType.Int, 4, ParameterDirection.Output, false, 10, 0, "", DataRowVersion.Proposed, m_iErrorCode));
-
-                sdaAdapter.Fill(dtToReturn);
-                return dtToReturn;
-            }
-            catch (Exception ex)
-            {
-                // some error occured. Bubble it to caller and encapsulate Exception object
-                throw new Exception("pr_DaiLy_ThamChieu_TinhXuatKho_Temp_SA_NgayThang", ex);
-            }
-            finally
-            {
-                //Close connection.
-                m_scoMainConnection.Close();
-                scmCmdToExecute.Dispose();
-                sdaAdapter.Dispose();
-            }
-        }
-        public DataTable SA_W_ID_XuatKhoDaiLy(int xxiID_XuatKhoDaiLy_)
-        {
-            SqlCommand scmCmdToExecute = new SqlCommand();
-            scmCmdToExecute.CommandText = "dbo.[pr_DaiLy_ThamChieu_TinhXuatKho_Temp_SA_W_ID_XuatKhoDaiLy]";
-            scmCmdToExecute.CommandType = CommandType.StoredProcedure;
-            DataTable dtToReturn = new DataTable("pr_DaiLy_ThamChieu_TinhXuatKho_Temp_SA_W_ID_XuatKhoDaiLy");
-            SqlDataAdapter sdaAdapter = new SqlDataAdapter(scmCmdToExecute);
-
-            // Use base class' connection object
-            scmCmdToExecute.Connection = m_scoMainConnection;
-
-            try
-            {
-                m_scoMainConnection.Open();
-               scmCmdToExecute.Parameters.Add(new SqlParameter("@iID_XuatKhoDaiLy", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, xxiID_XuatKhoDaiLy_));
                
                 sdaAdapter.Fill(dtToReturn);
                 return dtToReturn;
@@ -157,7 +104,7 @@ namespace CtyTinLuong
             catch (Exception ex)
             {
                 // some error occured. Bubble it to caller and encapsulate Exception object
-                throw new Exception("pr_DaiLy_ThamChieu_TinhXuatKho_Temp_SA_W_ID_XuatKhoDaiLy", ex);
+                throw new Exception("pr_GapDan_ThamChieuTinhXuatKho_Temp_SA_NgayThang", ex);
             }
             finally
             {
@@ -167,5 +114,6 @@ namespace CtyTinLuong
                 sdaAdapter.Dispose();
             }
         }
+       
     }
 }
