fa7e1ccd5c30c0b30f1a7d3b67ab51549b623d70
taquanghuu <huutaa88@gmail.com>
2021-07-11 13:27:21 +0700

    ssss

--- CtyTinLuong/QuanLySanXuat/Class/clsDinhMuc_ChiTiet_DinhMuc_ToGapDan - Copy.cs
+++ CtyTinLuong/QuanLySanXuat/Class/clsDinhMuc_ChiTiet_DinhMuc_ToGapDan - Copy.cs
@@ -7,7 +7,103 @@ namespace CtyTinLuong
 {
 	public partial class clsDinhMuc_ChiTiet_DinhMuc_ToGapDan : clsDBInteractionBase
 	{
-        //pr_DinhMuc_ChiTiet_DinhMuc_ToGapDan_SA_IDDM_W_SoLuong
+        public DataTable SA_ID_DinhMuc_VT_Phu(int iiID_Dinhmuc)
+        {
+            SqlCommand scmCmdToExecute = new SqlCommand();
+            scmCmdToExecute.CommandText = "dbo.[pr_DinhMuc_ChiTiet_DinhMuc_ToGapDan_SA_ID_DinhMuc_VT_Phu]";
+            scmCmdToExecute.CommandType = CommandType.StoredProcedure;
+            DataTable dtToReturn = new DataTable("pr_DinhMuc_ChiTiet_DinhMuc_ToGapDan_SA_ID_DinhMuc_VT_Phu");
+            SqlDataAdapter sdaAdapter = new SqlDataAdapter(scmCmdToExecute);
+
+            // Use base class' connection object
+            scmCmdToExecute.Connection = m_scoMainConnection;
+
+            try
+            {
+                m_scoMainConnection.Open();
+                scmCmdToExecute.Parameters.Add(new SqlParameter("@iID_DinhMuc_ToGapDan", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, iiID_Dinhmuc));
+                //scmCmdToExecute.Parameters.Add(new SqlParameter("@SoLuongXuat_", SqlDbType.Float, 8, ParameterDirection.Input, false, 38, 0, "", DataRowVersion.Proposed, xsoluongxuat_));
+                sdaAdapter.Fill(dtToReturn);
+                return dtToReturn;
+            }
+            catch (Exception ex)
+            {
+                // some error occured. Bubble it to caller and encapsulate Exception object
+                throw new Exception("pr_DinhMuc_ChiTiet_DinhMuc_ToGapDan_SA_ID_DinhMuc_VT_Phu", ex);
+            }
+            finally
+            {
+                //Close connection.
+                m_scoMainConnection.Close();
+                scmCmdToExecute.Dispose();
+                sdaAdapter.Dispose();
+            }
+        }
+
+        public DataTable SA_ID_DinhMuc_VT_Chinh(int iiID_Dinhmuc)
+        {
+            SqlCommand scmCmdToExecute = new SqlCommand();
+            scmCmdToExecute.CommandText = "dbo.[pr_DinhMuc_ChiTiet_DinhMuc_ToGapDan_SA_ID_DinhMuc_VT_Chinh]";
+            scmCmdToExecute.CommandType = CommandType.StoredProcedure;
+            DataTable dtToReturn = new DataTable("pr_DinhMuc_ChiTiet_DinhMuc_ToGapDan_SA_ID_DinhMuc_VT_Chinh");
+            SqlDataAdapter sdaAdapter = new SqlDataAdapter(scmCmdToExecute);
+
+            // Use base class' connection object
+            scmCmdToExecute.Connection = m_scoMainConnection;
+
+            try
+            {
+                m_scoMainConnection.Open();
+                scmCmdToExecute.Parameters.Add(new SqlParameter("@iID_DinhMuc_ToGapDan", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, iiID_Dinhmuc));
+                //scmCmdToExecute.Parameters.Add(new SqlParameter("@SoLuongXuat_", SqlDbType.Float, 8, ParameterDirection.Input, false, 38, 0, "", DataRowVersion.Proposed, xsoluongxuat_));
+                sdaAdapter.Fill(dtToReturn);
+                return dtToReturn;
+            }
+            catch (Exception ex)
+            {
+                // some error occured. Bubble it to caller and encapsulate Exception object
+                throw new Exception("pr_DinhMuc_ChiTiet_DinhMuc_ToGapDan_SA_ID_DinhMuc_VT_Chinh", ex);
+            }
+            finally
+            {
+                //Close connection.
+                m_scoMainConnection.Close();
+                scmCmdToExecute.Dispose();
+                sdaAdapter.Dispose();
+            }
+        }
+        public DataTable SA_ID_DinhMuc_ThanhPham( int iiID_Dinhmuc)
+        {
+            SqlCommand scmCmdToExecute = new SqlCommand();
+            scmCmdToExecute.CommandText = "dbo.[pr_DinhMuc_ChiTiet_DinhMuc_ToGapDan_SA_ID_DinhMuc_ThanhPham]";
+            scmCmdToExecute.CommandType = CommandType.StoredProcedure;
+            DataTable dtToReturn = new DataTable("pr_DinhMuc_ChiTiet_DinhMuc_ToGapDan_SA_ID_DinhMuc_ThanhPham");
+            SqlDataAdapter sdaAdapter = new SqlDataAdapter(scmCmdToExecute);
+
+            // Use base class' connection object
+            scmCmdToExecute.Connection = m_scoMainConnection;
+
+            try
+            {
+                m_scoMainConnection.Open();
+                scmCmdToExecute.Parameters.Add(new SqlParameter("@iID_DinhMuc_ToGapDan", SqlDbType.Int, 4, ParameterDirection.Input, false, 10, 0, "", DataRowVersion.Proposed, iiID_Dinhmuc));
+                //scmCmdToExecute.Parameters.Add(new SqlParameter("@SoLuongXuat_", SqlDbType.Float, 8, ParameterDirection.Input, false, 38, 0, "", DataRowVersion.Proposed, xsoluongxuat_));
+                sdaAdapter.Fill(dtToReturn);
+                return dtToReturn;
+            }
+            catch (Exception ex)
+            {
+                // some error occured. Bubble it to caller and encapsulate Exception object
+                throw new Exception("pr_DinhMuc_ChiTiet_DinhMuc_ToGapDan_SA_ID_DinhMuc_ThanhPham", ex);
+            }
+            finally
+            {
+                //Close connection.
+                m_scoMainConnection.Close();
+                scmCmdToExecute.Dispose();
+                sdaAdapter.Dispose();
+            }
+        }
         public DataTable SA_IDDM_W_SoLuong(double xsoluongxuat_, int iiID_Dinhmuc)
         {
             SqlCommand scmCmdToExecute = new SqlCommand();
--- CtyTinLuong/QuanLySanXuat/frmDinhMuc_ChiTietDinhMucToGapDan.cs
+++ CtyTinLuong/QuanLySanXuat/frmDinhMuc_ChiTietDinhMucToGapDan.cs
@@ -77,7 +77,7 @@ namespace CtyTinLuong
             }
         }
 
-        private void Luu_Sua__DM_NPL()
+        private void Luu_Sua__DM_NPL(int iiID_DinhMuc)
         {
             if (!KiemTraLuu()) return;
             else
@@ -92,17 +92,17 @@ namespace CtyTinLuong
 
                 cls1.bTonTai = true;
                 cls1.bNgungTheoDoi = false;
-                cls1.iID_DinhMuc_ToGapDan = UCSanXuat_DinhMuc_ToGapDan.miID_DinhMuc_ToGapDan;
+                cls1.iID_DinhMuc_ToGapDan = iiID_DinhMuc;
                 cls1.Update();
 
                 // XÓa trước
                 clsDinhMuc_ChiTiet_DinhMuc_ToGapDan cls2 = new clsDinhMuc_ChiTiet_DinhMuc_ToGapDan();
-                cls2.iID_DinhMuc_ToGapDan = UCSanXuat_DinhMuc_ToGapDan.miID_DinhMuc_ToGapDan;
+                cls2.iID_DinhMuc_ToGapDan = iiID_DinhMuc;
                 cls2.Delete_ALL_W_ID_DinhMuc_ToGapDan();
 
                 // insert thành phẩm
                 
-                cls2.iID_DinhMuc_ToGapDan = UCSanXuat_DinhMuc_ToGapDan.miID_DinhMuc_ToGapDan;
+                cls2.iID_DinhMuc_ToGapDan = iiID_DinhMuc;
                 cls2.iID_VTHH = Convert.ToInt16(gridMaTPQuyDoi.EditValue.ToString());
                 cls2.fSoLuong = 1;
                 cls2.bTonTai = true;
@@ -110,7 +110,7 @@ namespace CtyTinLuong
                 cls2.iInt_TP_1_Chinh_2_Phu_3 = 1;
                 cls2.Insert();
                 //insert vật tư chính
-                cls2.iID_DinhMuc_ToGapDan = UCSanXuat_DinhMuc_ToGapDan.miID_DinhMuc_ToGapDan;
+                cls2.iID_DinhMuc_ToGapDan = iiID_DinhMuc;
                 cls2.iID_VTHH = Convert.ToInt16(gridMaVTchinh1.EditValue.ToString());
                 cls2.fSoLuong = Convert.ToDouble(txtSoLuongVTChinh.Text.ToString());
                 cls2.bTonTai = true;
@@ -126,7 +126,7 @@ namespace CtyTinLuong
 
                 for (int i = 0; i < dv3.Rows.Count; i++)
                 {
-                    cls2.iID_DinhMuc_ToGapDan = UCSanXuat_DinhMuc_ToGapDan.miID_DinhMuc_ToGapDan; ;
+                    cls2.iID_DinhMuc_ToGapDan = iiID_DinhMuc; ;
                     cls2.iID_VTHH = Convert.ToInt16(dv3.Rows[i]["ID_VTHH"].ToString());
                     if (dv3.Rows[i]["SoLuong"].ToString() != "")
                         cls2.fSoLuong = Convert.ToDouble(dv3.Rows[i]["SoLuong"].ToString());
@@ -145,33 +145,6 @@ namespace CtyTinLuong
         private void HienThi_ThemMoi_DinhMuc_NPL()
         {
             dteNgayLap.EditValue = DateTime.Today;
-
-        }
-
-        private void HienThi_Sua_DinhMuc_NPL()
-        {
-            clsDinhMuc_DinhMuc_ToGapDan cls1 = new clsDinhMuc_DinhMuc_ToGapDan();
-            cls1.iID_DinhMuc_ToGapDan =  UCSanXuat_DinhMuc_ToGapDan.miID_DinhMuc_ToGapDan;
-            DataTable dt = cls1.SelectOne();
-            txtMaDinhMucNPL.Text = cls1.sMaDinhMuc.Value.ToString();
-            dteNgayLap.EditValue = cls1.daNgayLap.Value;
-            txtDienGiai.Text = cls1.sDienGiai.Value.ToString();
-            
-
-            clsDinhMuc_ChiTiet_DinhMuc_ToGapDan cls2 = new CtyTinLuong.clsDinhMuc_ChiTiet_DinhMuc_ToGapDan();
-            cls2.iID_DinhMuc_ToGapDan= UCSanXuat_DinhMuc_ToGapDan.miID_DinhMuc_ToGapDan;
-            DataTable dt322 = cls2.SelectAll_W_ID_DinhMuc_ToGapDan();
-            DataTable dt422 = dt322.Copy();
-            dt322.DefaultView.RowFilter = " Check_ThanhPham = True";
-            DataView dv1 = dt322.DefaultView;
-            DataTable dt22 = dv1.ToTable();
-            gridMaTPQuyDoi.EditValue = Convert.ToInt32(dt22.Rows[0]["ID_VTHH"].ToString());
-
-            dt422.DefaultView.RowFilter = " Check_VatTu_Chinh = True";
-            DataView dv2 = dt422.DefaultView;
-            DataTable dt223 = dv2.ToTable();
-            gridMaVTchinh1.EditValue = Convert.ToInt32(dt223.Rows[0]["ID_VTHH"].ToString());
-            txtSoLuongVTChinh.Text= dt223.Rows[0]["SoLuong"].ToString();
             DataTable dt2 = new DataTable();
             dt2.Columns.Add("ID_ChiTiet_DinhMuc_ToGapDan", typeof(int));
             dt2.Columns.Add("ID_DinhMuc_ToGapDan", typeof(int));
@@ -181,39 +154,29 @@ namespace CtyTinLuong
             dt2.Columns.Add("TenVTHH");
             dt2.Columns.Add("DonViTinh");
             dt2.Columns.Add("HienThi", typeof(string));
-            dt2.Columns.Add("Check_VatTu_Phu", typeof(bool));
-            //Check_VatTu_Phu
-
-
-            cls2.iID_DinhMuc_ToGapDan =  UCSanXuat_DinhMuc_ToGapDan.miID_DinhMuc_ToGapDan;
-            DataTable dtxxx = cls2.SelectAll_W_ID_DinhMuc_ToGapDan();
-            Double ddsoluong;
-            dtxxx.DefaultView.RowFilter = "TonTai=True and Check_VatTu_Phu=True";
-            DataView dv = dtxxx.DefaultView;
-            DataTable dt3 = dv.ToTable();
-
-            for (int i = 0; i < dt3.Rows.Count; i++)
-            {
-                if (dt3.Rows[i]["SoLuong"].ToString() == "")
-                    ddsoluong = 0;
-                else ddsoluong = Convert.ToDouble(dt3.Rows[i]["SoLuong"].ToString());
-                DataRow _ravi = dt2.NewRow();
-                _ravi["ID_ChiTiet_DinhMuc_ToGapDan"] = Convert.ToInt16(dt3.Rows[i]["ID_ChiTiet_DinhMuc_ToGapDan"].ToString());
-                _ravi["ID_DinhMuc_ToGapDan"] = Convert.ToInt16(dt3.Rows[i]["ID_DinhMuc_ToGapDan"].ToString());
-                _ravi["ID_VTHH"] = Convert.ToInt16(dt3.Rows[i]["ID_VTHH"].ToString());
-                int ID_VTHHxxxx= Convert.ToInt16(dt3.Rows[i]["ID_VTHH"].ToString());
-                clsTbVatTuHangHoa clsvth = new clsTbVatTuHangHoa();
-                clsvth.iID_VTHH = ID_VTHHxxxx;
-                DataTable dtvthh = clsvth.SelectOne();
-                _ravi["SoLuong"] = ddsoluong;
-                _ravi["MaVT"] = ID_VTHHxxxx;
-                _ravi["TenVTHH"] = clsvth.sTenVTHH.Value;
-                _ravi["DonViTinh"] = clsvth.sDonViTinh.Value;
-                _ravi["HienThi"] = "1";
-                dt2.Rows.Add(_ravi);
-            }
             gridControl1.DataSource = dt2;
+        }
 
+        private void HienThi_Sua_DinhMuc_NPL(int iiID_DinhMuc)
+        {
+            clsDinhMuc_DinhMuc_ToGapDan cls1 = new clsDinhMuc_DinhMuc_ToGapDan();
+            cls1.iID_DinhMuc_ToGapDan =  iiID_DinhMuc;
+            DataTable dt = cls1.SelectOne();
+            txtMaDinhMucNPL.Text = cls1.sMaDinhMuc.Value.ToString();
+            dteNgayLap.EditValue = cls1.daNgayLap.Value;
+            txtDienGiai.Text = cls1.sDienGiai.Value.ToString();
+            
+
+            clsDinhMuc_ChiTiet_DinhMuc_ToGapDan cls2 = new CtyTinLuong.clsDinhMuc_ChiTiet_DinhMuc_ToGapDan();
+            DataTable dt_chinh = cls2.SA_ID_DinhMuc_VT_Chinh(iiID_DinhMuc);
+            DataTable dt_Phu = cls2.SA_ID_DinhMuc_VT_Phu(iiID_DinhMuc);
+            DataTable dt_ThanhPham = cls2.SA_ID_DinhMuc_ThanhPham(iiID_DinhMuc);
+
+            gridControl1.DataSource = dt_Phu;
+            gridMaVTchinh1.EditValue = Convert.ToInt32(dt_chinh.Rows[0]["ID_VTHH"].ToString());
+            gridMaTPQuyDoi.EditValue = Convert.ToInt32(dt_ThanhPham.Rows[0]["ID_VTHH"].ToString());
+
+          
         }
 
         private bool KiemTraLuu()
@@ -252,16 +215,7 @@ namespace CtyTinLuong
             Cursor.Current = Cursors.WaitCursor;
             try
             {
-                DataTable dt2 = new DataTable();
-                dt2.Columns.Add("ID_ChiTiet_DinhMuc_ToGapDan", typeof(int));
-                dt2.Columns.Add("ID_DinhMuc_ToGapDan", typeof(int));
-                dt2.Columns.Add("ID_VTHH", typeof(int));
-                dt2.Columns.Add("SoLuong", typeof(float));
-                dt2.Columns.Add("MaVT");// 
-                dt2.Columns.Add("TenVTHH");
-                dt2.Columns.Add("DonViTinh");
-                dt2.Columns.Add("HienThi", typeof(string));
-                gridControl1.DataSource = dt2;
+             
 
                 clsTbVatTuHangHoa cls = new clsTbVatTuHangHoa();
                 DataTable dt = cls.T_SelectAll(); 
@@ -281,7 +235,7 @@ namespace CtyTinLuong
                 cls.Dispose();
                 if (UCSanXuat_DinhMuc_ToGapDan.mb_TheMoi_DinhMuc_ToGapDan == true)
                     HienThi_ThemMoi_DinhMuc_NPL();
-                else HienThi_Sua_DinhMuc_NPL();
+                else HienThi_Sua_DinhMuc_NPL(UCSanXuat_DinhMuc_ToGapDan.miID_DinhMuc_ToGapDan);
             }
             catch { }
             
@@ -313,7 +267,7 @@ namespace CtyTinLuong
             Cursor.Current = Cursors.WaitCursor;
             if (UCSanXuat_DinhMuc_ToGapDan.mb_TheMoi_DinhMuc_ToGapDan == true)
                 Luu_ThemMoi_DM_NPL();
-            else Luu_Sua__DM_NPL();
+            else Luu_Sua__DM_NPL(UCSanXuat_DinhMuc_ToGapDan.miID_DinhMuc_ToGapDan);
             Cursor.Current = Cursors.Default;
         }
 
