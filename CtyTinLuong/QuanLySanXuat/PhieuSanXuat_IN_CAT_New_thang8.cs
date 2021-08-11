4dad41e92d6f4b929f6c2b1ad7c0512febda615e
huutq-88 <84846147+huutq-88@users.noreply.github.com>
2021-08-11 10:50:39 +0700

    s

---
+++ CtyTinLuong/QuanLySanXuat/PhieuSanXuat_IN_CAT_New_thang8.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CtyTinLuong
+{
+    public partial class PhieuSanXuat_IN_CAT_New_thang8 : Form
+    {
+        public PhieuSanXuat_IN_CAT_New_thang8()
+        {
+            InitializeComponent();
+        }
+    }
+}
