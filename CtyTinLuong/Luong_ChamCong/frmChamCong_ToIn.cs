1e82a58e406c27e1fa3aa4546b02dfc5a23cc9d4
thinmta <31883027+thinmta@users.noreply.github.com>
2021-10-01 07:44:52 +0700

    a

--- CtyTinLuong/Luong_ChamCong/frmChamCong_ToIn.cs
+++ CtyTinLuong/Luong_ChamCong/frmChamCong_ToIn.cs
@@ -32,6 +32,8 @@ namespace CtyTinLuong
 
         frmQuanLy_Luong_ChamCong _frmQLLCC;
 
+        DevExpress.XtraEditors.Repository.RepositoryItemButtonEdit emptyEditor;
+
         public frmChamCong_ToIn(int id_bophan, frmQuanLy_Luong_ChamCong frmQLLCC)
         {
             _frmQLLCC = frmQLLCC;
@@ -54,6 +56,12 @@ namespace CtyTinLuong
 
             this.cbNhanSu.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
             this.cbNhanSu.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+
+
+            emptyEditor = new DevExpress.XtraEditors.Repository.RepositoryItemButtonEdit();
+            emptyEditor.Buttons.Clear();
+            emptyEditor.TextEditStyle = DevExpress.XtraEditors.Controls.TextEditStyles.HideTextEditor;
+            gridControl1.RepositoryItems.Add(emptyEditor);
         }
         public void Load_DinhMuc(int id_dinhmuc,string ma,int id_congnhan)
         {
@@ -1091,6 +1099,17 @@ namespace CtyTinLuong
             catch (Exception ee)
             {
                 MessageBox.Show("Error xóa công nhân khỏi bảng..." + ee.ToString(), "Error!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void gridView1_CustomRowCellEdit(object sender, CustomRowCellEditEventArgs e)
+        {
+            if (e.RowHandle == _data.Rows.Count - 1)
+            {
+                if (e.Column.FieldName == "Xoa")
+                {
+                    e.RepositoryItem = emptyEditor;
+                }
             }
         }
 
